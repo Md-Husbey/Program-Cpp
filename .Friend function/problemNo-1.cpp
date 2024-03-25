7a0324397818ba2d67578a234f19e18b4e40c9f2
Husbey <mdhusbeyhowlader916761@gmail.com>
2024-03-25 14:19:00 -0600

    new output

---
+++ .Friend function/problemNo-1.cpp
@@ -0,0 +1,36 @@
+/*1. Define a class Rectangle with private members for length and width. Write a
+friend function findArea that calculates and returns the area of the rectangle.*/
+#include<iostream>
+using namespace std;
+
+class A
+{
+    float area,length,circumference;
+
+public:
+    void get(float L)
+    {
+        length=L;
+        circumference=L*3;
+        area=(1.73*length*length)/4;
+
+    }
+
+    friend void dispay( A);
+};
+void dispay( A obj)
+{
+    cout<<"circumference IS = "<<obj.circumference<<endl;
+    cout<<"Area IS = "<<obj.area<<endl;
+
+}
+
+int main()
+{
+    A Obj;
+
+    Obj.get(10);
+    dispay(Obj);
+
+    return 0;
+}
