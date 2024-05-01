57d48fe27de36d12b56b877058b6978607397e22
Husbey <mdhusbeyhowlader916761@gmail.com>
2024-05-01 10:44:33 -0600

    new input

---
+++ c++/arry.cpp
@@ -0,0 +1,48 @@
+// C++ program to implement
+// the above approach
+#include<iostream>
+using namespace std;
+
+class Employee
+{
+int id;
+char name[30];
+public:
+
+// Declaration of function
+void getdata(){
+cout << "Enter Id : ";
+cin >> id;
+cout << "Enter Name : ";
+cin >> name;
+}
+
+// Declaration of function
+void putdata(){
+cout << id << " ";
+cout << name << " ";
+cout << endl;
+}
+};
+
+
+int main()
+{
+// This is an array of objects having
+// maximum limit of 30 Employees
+Employee emp[30];
+int n, i;
+cout << "Enter Number of Employees - ";
+cin >> n;
+
+// Accessing the function
+for(i = 0; i < n; i++)
+	emp[i].getdata();
+
+cout << "Employee Data - " << endl;
+
+// Accessing the function
+for(i = 0; i < n; i++)
+	emp[i].putdata();
+}
+
