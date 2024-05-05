f288ddf64d564309fdf0d0e19548db757b6c9246
Husbey <mdhusbeyhowlader916761@gmail.com>
2024-05-05 13:59:57 -0600

    new input

--- .arryAndObj/p_2.cpp
+++ .arryAndObj/p_2.cpp
@@ -6,57 +6,49 @@ automatically assigned.*/
 
 using namespace std;
 
-class Student
-{
+class Student {
 public:
     int rollNumber;
     float marks[3];
-    void inputMarks()
-    {
-        cout << "Enter marks for student with roll number " << rollNumber << endl;
-        for (int i = 0; i < 3; i++)
-        {
+    void inputMarks() {
+        cout << "Enter marks for student with roll number " << rollNumber << ":" << endl;
+        for (int i = 0; i < 3; i++) {
             cout << "Enter marks for subject " << (i + 1) << ": ";
             cin >> marks[i];
         }
     }
 
 
-    float calculateAverage()
-    {
+    float calculateAverage() {
         float total = 0;
-        for (int i = 0; i < 3; i++)
-        {
+        for (int i = 0; i < 3; i++) {
             total += marks[i];
         }
         return total / 3.0;
     }
 
 
-    void displayDetails()
-    {
+    void displayDetails() {
         cout << "Roll Number: " << rollNumber << endl;
         cout << "Average Marks: " << calculateAverage() << endl;
     }
 };
 
-int main()
-{
+int main() {
 
     Student students[8];
 
 
-    for (int i = 0; i < 8; i++)
-    {
+    for (int i = 0; i < 8; i++) {
         students[i].inputMarks();
     }
 
 
-
-    for (int i = 0; i < 8; i++)
-    {
+    cout << "Student Details:" << endl;
+    cout << "-----------------------------" << endl;
+    for (int i = 0; i < 8; i++) {
         students[i].displayDetails();
-
+        cout << "-----------------------------" << endl;
     }
 
     return 0;
